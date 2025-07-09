147f4cfab0161c789bb64d63cf5f783be9dc089d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 18:57:00 +0100

    Initial commit

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,4 +6,5 @@ export const ENV = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     MONGO_URI: process.env.MONGO_URI,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
 };
