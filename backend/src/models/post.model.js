64cd6292d1fbb5d8c85ce0244c7e7081904443ce
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 21:09:06 +0100

    Initial commit

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -4,6 +4,7 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
+        required: true
     },
 }, { timestamps: true });
 
