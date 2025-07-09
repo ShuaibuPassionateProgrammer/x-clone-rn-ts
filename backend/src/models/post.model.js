036e0a35a0e016485b52cabc1f7f6e501682215d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 21:08:41 +0100

    Initial commit

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    user: {},
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
 }, { timestamps: true });
 
 const Post = mongoose.model("Post", postSchema);
