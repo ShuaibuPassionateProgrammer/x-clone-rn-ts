e3c0caf4d0bc6d9d03548780817583f2e7a2ace8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 20:32:04 +0100

    Initial commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const conn = mongoose.connect(ENV.MONGO_URI);
+        const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }
     catch (error) {
