0954f07aeea4476f2164911b6d234ab04788e5f9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 20:52:37 +0100

    Initial commit

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -16,4 +16,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 });
