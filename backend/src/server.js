3d2aa7b4863380a1d946e16bd43987e3baad03c6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 18:53:49 +0100

    Initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,4 +3,4 @@ import { ENV } from "./config/env.js"
 
 const app = express();
 
-app.listen(ENV.port, () => console.log("Server running on PORT:5001"));
+app.listen(ENV.port, () => console.log("Server running on PORT:", ENV.port));
